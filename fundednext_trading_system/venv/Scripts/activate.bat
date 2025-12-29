22a5e8cce5647f85230c43b863b1760da14e0c07
PMB-Cyber <paulmbithimutuku@gmail.com>
2025-12-29 17:14:09 +0300

    Commit

--- fundednext_trading_system/venv/Scripts/activate.bat
+++ fundednext_trading_system/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\GemstoneCapital\Desktop\fundednext\fundednext_trading_system\venv
+set VIRTUAL_ENV=C:\Users\GemstoneCapital\Documents\GitHub\Gemstonecapital-FunedNext\fundednext_trading_system\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
